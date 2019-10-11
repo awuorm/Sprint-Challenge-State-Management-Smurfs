e309b09ce85929ec6c9141b35ca17f5bbada7185
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-11 10:53:47 +0300

    fetched data from api

--- smurfs/src/state/actionCreators.js
+++ smurfs/src/state/actionCreators.js
@@ -1,16 +1,40 @@
 import * as types from "./actionTypes";
+import axios from "axios";
 
 export const fetchData = () => {
     return { type: types.FETCH_DATA };
   };
   
-  export const getError = err => {
+  export const getError = (err) => {
     return { type: types.GET_ERROR, payload: err };
   };
 
   export const smurfData = () => dispatch => {
-      return {type: types.SMURF_DATA}
+    dispatch(fetchData());
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+          console.log("response from server", res);
+        dispatch({ type: types.SMURF_DATA, payload: res.data });
+      })
+      .catch(err => {
+        console.log("error from server", err);
+        dispatch(getError(err));
+      });
+
   }
+
+//   export const getCat = () => dispatch => {
+//     dispatch(fetchData());
+//     axios
+//       .get("https://reqres.in/api/users?page=2")
+//       .then(res => {
+//         dispatch({ type: types.GET_CAT, payload: res.data.data });
+//       })
+//       .catch(err => {
+//         dispatch(getError(err));
+//       });
+//   };
 
   export const postSmurf = () => dispatch => {
     return {type: types.POST_SMURF}
